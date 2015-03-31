7bffabdcaa06bf68af9bc6a7d8354ff6e9b81065
Serj <waycompass@gmail.com>
2015-03-31 19:11:05 +0300

    дрочь с анимацией скиллов

---
+++ libs/jqbar/bar.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+            $('#bar-1').jqbar({ label: 'HTML', value: 99, barColor: '#D64747' });
+
+            $('#bar-2').jqbar({ label: 'CSS', value: 70, barColor: '#FF681F' });
+
+            $('#bar-3').jqbar({ label: 'PS', value: 50, barColor: '#ea805c' });
+
+            $('#bar-4').jqbar({ label: 'SCSS', value: 40, barColor: '#88bbc8' });
+
+            $('#bar-5').jqbar({ label: 'Bootstrap', value: 60, barColor: '#939393' });
+
+            $('#bar-6').jqbar({ label: 'GIT', value: 40, barColor: '#3a89c9' });
+
+
+});
